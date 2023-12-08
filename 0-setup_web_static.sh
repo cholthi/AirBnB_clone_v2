920a4161c89ff61a75729f60920e2cd7944bdc58
Samuel Amihere <amisam2000@gmail.com>
2023-12-08 17:27:01 +0800

    Deploy static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -26,6 +26,5 @@ sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 
 #configure nginx
-str="listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}"
-sudo sed -i '/$str' /etc/nginx/sites-enabled/default
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 sudo service nginx restart
