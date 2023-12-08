3b08c38a6353ca8ec14b6e911474587f23731763
Samuel Amihere <amisam2000@gmail.com>
2023-12-08 19:01:35 +0800

    Deploy static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 from fabric.api import local
-from time import strftime
 from datetime import date
+import time
 
 
 def do_pack():
-    """ A script that generates archive the contents of web_static folder"""
+    """ A script to generate archive the contents of web_static folder"""
 
-    filename = strftime("%Y%m%d%H%M%S")
+    tme = strftime("%Y%m%d%H%M%S")
+    filename = None
     try:
         local("mkdir -p versions")
         local("tar -czvf versions/web_static_{}.tgz web_static/"
-              .format(filename))
+              .format(tme))
 
-        return "versions/web_static_{}.tgz".format(filename)
+        filename = "versions/web_static_{}.tgz".format(tme)
 
-    except Exception as e:
-        return None
+    except Exception:
+        pass
+    return filename
