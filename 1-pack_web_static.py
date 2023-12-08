e1c739c51df1b5f2ce147c42dd322a6ca535528e
Samuel Amihere <amisam2000@gmail.com>
2023-12-08 19:04:42 +0800

    Deploy static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,8 +7,8 @@ import time
 def do_pack():
     """ A script to generate archive the contents of web_static folder"""
 
-    tme = strftime("%Y%m%d%H%M%S")
-    filename = ""
+    tme = time.strftime("%Y%m%d%H%M%S")
+    filename = None
     try:
         local("mkdir -p versions")
         local("tar -czvf versions/web_static_{}.tgz web_static/"
@@ -17,5 +17,5 @@ def do_pack():
         filename = "versions/web_static_{}.tgz".format(tme)
 
     except Exception:
-        return None
+        pass
     return filename
