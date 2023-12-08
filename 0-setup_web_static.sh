05f35312535028f2da9f29c767579bb75bbbb6b7
cholthi <cholthipaul@gmail.com>
2023-12-08 11:37:55 +0200

    added 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -17,6 +17,6 @@ echo "<html><head>
       </html>" | tee /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data
-sed -i '11 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n'  /etc/nginx/sites-enabled/default
+sed -i '12 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n'  /etc/nginx/sites-enabled/default
 service nginx restart
 
