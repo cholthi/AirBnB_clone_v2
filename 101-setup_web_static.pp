f51e5577363164edf9f158186532b5f631248bba
Samuel Amihere <amisam2000@gmail.com>
2023-12-10 21:47:29 +0800

    uppet for setup!

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,18 +1,22 @@
 # Puppet for setups for server
 
+# Nginx config file
 $nginx_conf = "server {
     listen 80 default_server;
     listen [::]:80 default_server;
     add_header X-Served-By ${hostname};
     root   /var/www/html;
     index  index.html index.htm;
+
     location /hbnb_static {
         alias /data/web_static/current;
         index index.html index.htm;
     }
+
     location /redirect_me {
-        return 301 http://linktr.ee/firdaus_h_salim/;
+        return 301 http://cuberule.com/;
     }
+
     error_page 404 /404.html;
     location /404 {
       root /var/www/html;
@@ -27,33 +31,33 @@ package { 'nginx':
 
 file { '/data':
   ensure  => 'directory'
-} ->
+}
 
 file { '/data/web_static':
   ensure => 'directory'
-} ->
+}
 
 file { '/data/web_static/releases':
   ensure => 'directory'
-} ->
+}
 
 file { '/data/web_static/releases/test':
   ensure => 'directory'
-} ->
+}
 
 file { '/data/web_static/shared':
   ensure => 'directory'
-} ->
+}
 
 file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
   content => "I am found in data/web_static/releases/test/index.htm \n"
-} ->
+}
 
 file { '/data/web_static/current':
   ensure => 'link',
   target => '/data/web_static/releases/test'
-} ->
+}
 
 exec { 'chown -R ubuntu:ubuntu /data/':
   path => '/usr/bin/:/usr/local/bin/:/bin/'
@@ -61,11 +65,11 @@ exec { 'chown -R ubuntu:ubuntu /data/':
 
 file { '/var/www':
   ensure => 'directory'
-} ->
+}
 
 file { '/var/www/html':
   ensure => 'directory'
-} ->
+}
 
 -> file { '/var/www/html/index.html':
   ensure  => 'present',
@@ -75,12 +79,12 @@ file { '/var/www/html':
 file { '/var/www/html/404.html':
   ensure  => 'present',
   content => "Ceci n'est pas une page\n"
-} ->
+}
 
 file { '/etc/nginx/sites-available/default':
   ensure  => 'present',
   content => $nginx_conf
-} ->
+}
 
 exec { 'nginx restart':
   path => '/etc/init.d/'
