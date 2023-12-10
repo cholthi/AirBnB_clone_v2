8122c1adbf7bbca93b0eadc5b33e791c6eea9afe
Samuel Amihere <amisam2000@gmail.com>
2023-12-10 21:35:39 +0800

    uppet for setup!

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,21 +1,18 @@
 # Puppet for setups for server
 
-$nginx_config = "server {
+$nginx_conf = "server {
     listen 80 default_server;
     listen [::]:80 default_server;
     add_header X-Served-By ${hostname};
     root   /var/www/html;
     index  index.html index.htm;
-
     location /hbnb_static {
         alias /data/web_static/current;
         index index.html index.htm;
     }
-
     location /redirect_me {
-        return 301 http://cuberule.com/;
+        return 301 http://linktr.ee/firdaus_h_salim/;
     }
-
     error_page 404 /404.html;
     location /404 {
       root /var/www/html;
@@ -26,7 +23,7 @@ $nginx_config = "server {
 package { 'nginx':
   ensure   => 'present',
   provider => 'apt'
-} ->
+}
 
 file { '/data':
   ensure  => 'directory'
