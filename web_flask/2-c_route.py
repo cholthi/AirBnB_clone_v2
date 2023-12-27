ff75f4b15e2f9351c9492ccd7463b818dc790e9a
Samuel Amihere <amisam2000@gmail.com>
2023-12-27 23:51:53 +0800

    flask

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+starting a Flask app
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """
+    returns Hello HBNB!
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    returns 'HBNB'
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def displayC():
+    """
+    returns 'C' followed by the value of the text
+    """
+    txt = text.replace('_', ' ')
+    return "C " + txt
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
