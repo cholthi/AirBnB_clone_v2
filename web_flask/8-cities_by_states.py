89ae193ab2b6987b223fa251c77ad64b6119e567
Samuel Amihere <amisam2000@gmail.com>
2023-12-31 04:09:16 +0800

    update

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -9,10 +9,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route('/cities_by_states', strict_slashes=False)
 def states_list():
     """
-    display a HTML page
+    display a HTML page for cities_by_states
     """
     all_states = storage.all(State)
     return render_template('8-cities_by_states.html', all_states=all_states)
