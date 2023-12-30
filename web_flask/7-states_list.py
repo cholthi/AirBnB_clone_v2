854ab7ed51b7431a5da88c61c08d44e3998dfc1a
Samuel Amihere <amisam2000@gmail.com>
2023-12-31 03:48:00 +0800

    update

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,27 +4,28 @@ starting a Flask app
 """
 
 from models import storage
-from models import *
+from models.state import State
 from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
-def list_states():
+def states_list():
     """
     display a HTML page
     """
-    all_states = sorted(list(storage.all("State").values()),
-            key=lambda x: x.name)
+    all_states = sorted(storage.all(State).values(),
+            key=lambda state: state.name)
     return render_template('7-states_list.html', all_states=all_states)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
+def teardown_db(arg=None):
     """
     remove the current db Session
     """
     storage.close()
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
