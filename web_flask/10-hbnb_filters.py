87d822201c886461ae810136ef4ec102d6e8cc44
Samuel Amihere <amisam2000@gmail.com>
2023-12-31 07:40:41 +0800

    update

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+starting a Flask app
+"""
+
+from models import storage
+from models.amenity import Amenity
+from models.state import State
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    """Render template with all the states and their amenities
+    """
+    html_path = '10-hbnb_filters.html'
+    all_states = storage.all(State)
+    all_amenities = storage.all(Amenity)
+    return render_template(html_path, all_states=all_states,
+                           all_amenities=all_amenities)
+
+
+@app.teardown_appcontext
+def teardown_db(arg=None):
+    """
+    remove the current db Session
+    """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
