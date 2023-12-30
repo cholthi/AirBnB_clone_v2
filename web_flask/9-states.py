7cd3020221dd12d7a0bac88f4be7b2580f9ae18f
Samuel Amihere <amisam2000@gmail.com>
2023-12-31 04:50:50 +0800

    update

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,44 +1,33 @@
 #!/usr/bin/python3
-"""
-starting a Flask app
-"""
-
+"""Importing Flask to run the web app"""
+from flask import Flask, render_template
 from models import storage
 from models.state import State
-from flask import Flask, render_template
+
+
 app = Flask(__name__)
 
 
+@app.teardown_appcontext
+def close(self):
+    """ Method to close the session """
+    storage.close()
+
+
 @app.route('/states', strict_slashes=False)
-def states_list():
-    """
-    display a HTML page for /states
-    """
-    all_states = storage.all(State)
-    return render_template('9-states.html',
-                           all_states=all_states, mode='all')
+def state():
+    """Displays a html page with states"""
+    states = storage.all(State)
+    return render_template('9-states.html', states=states, mode='all')
 
 
 @app.route('/states/<id>', strict_slashes=False)
 def state_by_id(id):
-    """
-    display a HTML page
-    """
-    all_states = storage.all(State)
-    for state in all_states:
+    """Displays a html page with citys of that state"""
+    for state in storage.all(State).values():
         if state.id == id:
-            return render_template('9-states.html',
-                                   all_states=all_states, mode='id')
-    return render_template('9-states.html',
-                           all_states=all_states, mode='none')
-
-
-@app.teardown_appcontext
-def teardown_db(arg=None):
-    """
-    remove the current db Session
-    """
-    storage.close()
+            return render_template('9-states.html', states=state, mode='id')
+    return render_template('9-states.html', states=state, mode='none')
 
 
 if __name__ == '__main__':
