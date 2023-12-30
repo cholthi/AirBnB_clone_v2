0904b79edee61507ac1ba08a35997992c12f3618
Samuel Amihere <amisam2000@gmail.com>
2023-12-31 02:57:18 +0800

    update

--- models/state.py
+++ models/state.py
@@ -12,27 +12,21 @@ import shlex
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade='all, delete, delete-orphan',
-                          backref="state")
 
     if environ.get('HBNB_TYPE_STORAGE') == "db":
-        cities = relationship("City", backref="state",
-                              cascade="all, delete, delete-orphan")
-
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
     else:
+        name = ""
+
+    if environ.get('HBNB_TYPE_STORAGE') != "db":
         @property
         def cities(self):
-            var = models.storage.all()
-            lista = []
-            result = []
-            for key in var:
-                city = key.replace('.', ' ')
-                city = shlex.split(city)
-                if (city[0] == 'City'):
-                    lista.append(var[key])
-            for elem in lista:
-                if (elem.state_id == self.id):
-                    result.append(elem)
-            return (result)
+            """getter for list of city instances related to the state"""
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
